9a71ef7c724d0f07d14507ba565367c34dc8d85c
Jason Godesky <jefgodesky@gmail.com>
2025-02-09 20:51:46 -0500

    patchScale — Also update authors

--- utils/patching/scale.test.ts
+++ utils/patching/scale.test.ts
@@ -1,7 +1,7 @@
 import { describe, it } from 'jsr:@std/testing/bdd'
 import { expect } from 'jsr:@std/expect'
 import { createScale } from '../../types/scale.ts'
-import type ScalePatch from '../../types/scale-patch.ts'
+import UserResource, { createUserResource } from '../../types/user-resource.ts'
 import patchScale from './scale.ts'
 
 describe('patchScale', () => {
@@ -11,21 +11,54 @@ describe('patchScale', () => {
   orig.created = new Date('2025-01-01')
   orig.updated = new Date('2025-01-01')
 
-  const patch: ScalePatch = {
-    data: {
-      type: 'scales',
-      id: orig.id ?? 'ERROR',
-      attributes: { name, levels }
-    }
-  }
-
   it('patches a scale', () => {
-    const patched = patchScale(orig, patch)
+    const patched = patchScale(orig, {
+      data: {
+        type: 'scales',
+        id: orig.id ?? 'ERROR',
+        attributes: { name, levels }
+      }
+    })
+
     expect(patched.id).toBe(orig.id)
     expect(patched.name).toBe(name)
     expect(patched.levels).toEqual(levels)
     expect(patched.description).toBe(orig.description)
+    expect(patched.authors).toEqual(orig.authors)
     expect(patched.created).toEqual(orig.created)
     expect((patched.updated as Date).getTime()).toBeGreaterThan((orig.updated as Date).getTime())
+  })
+
+  it('adds authors', () => {
+    const patched = patchScale(orig, {
+      data: {
+        type: 'scales',
+        id: orig.id ?? 'ERROR',
+        attributes: {},
+        relationships: {
+          authors: {
+            data: [
+              ...orig.authors.map(author => ({ type: 'users', id: author.id ?? 'ERROR' } as UserResource)),
+              createUserResource()
+            ]
+          }
+        }
+      }
+    })
+
+    expect(patched.authors).toHaveLength(2)
+  })
+
+  it('removes authors', () => {
+    const patched = patchScale(orig, {
+      data: {
+        type: 'scales',
+        id: orig.id ?? 'ERROR',
+        attributes: {},
+        relationships: { authors: { data: [] } }
+      }
+    })
+
+    expect(patched.authors).toHaveLength(0)
   })
 })
--- utils/patching/scale.ts
+++ utils/patching/scale.ts
@@ -1,6 +1,7 @@
 import type Scale from '../../types/scale.ts'
 import type ScaleAttributes from '../../types/scale-attributes.ts'
 import type ScalePatch from '../../types/scale-patch.ts'
+import UserResource from '../../types/user-resource.ts'
 
 const patchScale = (scale: Scale, patch: ScalePatch): Scale => {
   const patched = structuredClone(scale)
@@ -8,6 +9,11 @@ const patchScale = (scale: Scale, patch: ScalePatch): Scale => {
   const patchedKeys = Object.keys(patch.data.attributes) as (keyof ScaleAttributes)[]
   for (const key of patchedKeys) {
     patched[key] = patch.data.attributes[key] as string & string[] & Date
+  }
+
+  if (patch.data.relationships?.authors?.data) {
+    const resources = structuredClone(patch.data.relationships.authors.data) as UserResource[]
+    patched.authors = resources.map(res => ({ id: res.id, name: '' }))
   }
 
   patched.updated = new Date()
