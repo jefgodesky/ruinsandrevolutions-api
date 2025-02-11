a37f20500535ca639bb13a2f5f289d58e58a7e37
Jason Godesky <jefgodesky@gmail.com>
2025-02-10 21:54:05 -0500

    Middlewares / loadItemCreation: Support ScrollCreation

--- middlewares/load/item-creation.test.ts
+++ middlewares/load/item-creation.test.ts
@@ -2,15 +2,23 @@ import { describe, it } from 'jsr:@std/testing/bdd'
 import { expect } from 'jsr:@std/expect'
 import { createMockContext, createMockNext } from '@oak/oak/testing'
 import { createScaleCreation } from '../../types/scale-creation.ts'
+import { createScrollCreation } from '../../types/scroll-creation.ts'
 import stringToReadableStream from '../../utils/transformers/string-to/readable-stream.ts'
 import loadItemCreation from './item-creation.ts'
 
 describe('loadItemCreation', () => {
-  it('loads ScaleCreation body into state', async () => {
-    const ctx = createMockContext({
-      body: stringToReadableStream(JSON.stringify(createScaleCreation()))
-    })
-    await loadItemCreation(ctx, createMockNext())
-    expect(ctx.state.itemCreation).toBeDefined()
+  it('loads item creation body into state', async () => {
+    const posts = [
+      createScaleCreation(),
+      createScrollCreation()
+    ]
+
+    for (const post of posts) {
+      const ctx = createMockContext({
+        body: stringToReadableStream(JSON.stringify(post))
+      })
+      await loadItemCreation(ctx, createMockNext())
+      expect(ctx.state.itemCreation).toBeDefined()
+    }
   })
 })
--- middlewares/load/item-creation.ts
+++ middlewares/load/item-creation.ts
@@ -1,10 +1,12 @@
 import { Middleware } from '@oak/oak'
 import { isScaleCreation } from '../../types/scale-creation.ts'
+import { isScrollCreation } from '../../types/scroll-creation.ts'
 
 const loadItemCreation: Middleware = async (ctx, next) => {
   const body = await ctx.request.body.json()
   const tests = [
-    isScaleCreation(body)
+    isScaleCreation(body),
+    isScrollCreation(body)
   ]
 
   if (tests.some(test => test === true)) ctx.state.itemCreation = body
